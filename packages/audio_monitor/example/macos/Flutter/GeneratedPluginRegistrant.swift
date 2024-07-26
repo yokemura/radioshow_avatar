4dbe83406fa2288ac9ef93fb86d6b901b3ee04ae
Takeshi Yokemura <yokemura@ymck.net>
2024-07-26 21:16:06 +0900

    Generated files

---
+++ packages/audio_monitor/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audio_monitor
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioMonitorPlugin.register(with: registry.registrar(forPlugin: "AudioMonitorPlugin"))
+}
