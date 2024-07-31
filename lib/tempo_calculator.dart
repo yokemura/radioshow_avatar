17c875978b06c1a458bd5fa39d076a5b854c3b8f
Takeshi Yokemura <yokemura@ymck.net>
2024-07-31 23:10:00 +0900

    vars and consts

---
+++ lib/tempo_calculator.dart
@@ -0,0 +1,10 @@
+class TempoCalculator {
+  static const int maxTap = 10;
+  static const double minBpm = 60;
+  static const double maxBpm = 200;
+
+  List<DateTime> taps = [];
+  double bpm = minBpm;
+
+  
+}
